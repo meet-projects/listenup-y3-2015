114db56b023cd398233052c0595cba9808266dde
Amit Nahari <amit13@meet.mit.edu>
2015-03-16 19:56:07 +0200

    Templates done

--- project/app/urls.py
+++ project/app/urls.py
@@ -6,4 +6,7 @@ from app import views
 urlpatterns = patterns('',
     url(r'^home/$', views.home, name='home'),
     url(r'^$', views.index, name='index'),
+    url(r'^signup/$', views.signup, name='signup'),
+    url(r'^login/$', views.login, name='login'),
+    url(r'^achievements/$', views.achievements, name='achievements'),
 )
--- project/app/views.py
+++ project/app/views.py
@@ -5,3 +5,9 @@ def index(request):
 	return render(request, 'app/index.html', {})
 def home(request):
 	return render(request, 'app/home.html', {})
+def signup(request):
+	return render(request, 'app/signup.html', {})
+def login(request):
+	return render(request, 'app/login.html', {})
+def achievements(request):
+	return render(request, 'app/achievements.html', {})
